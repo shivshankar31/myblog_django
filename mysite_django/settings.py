374222375d1c544f2a4e7924fcd8e3da95024b88
shivshankar31 <shivshankar31@yahoo.com>
2021-11-24 14:04:58 +0530

    58 step 4: create css file and edit index html file

--- mysite_django/settings.py
+++ mysite_django/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# step 4.4: add staticfile_dirs in settings.py to inform django that we have created static folder globaly.
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
