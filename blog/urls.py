55b5eb71063250ac9d59d1f886ab0dfcb1907dbd
shivshankar31 <shivshankar31@yahoo.com>
2021-11-24 23:05:01 +0530

    62 step6: detail page added with style and view function is enabled temp

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@
 # step 2.3: import views and assign view.function name to call the function inside urlpattens
 # step 2.4: assign name for each url path.
 
+
 from django.urls import path
 from .import views
 
@@ -9,5 +10,5 @@ from .import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('blogs', views.post, name='blogs'),
-    path('blogs/<slug:slug>', views.post_Details, name='blog_details')
+    path('blogs/<slug:slug>', views.detail_post, name='blog_details')
 ]
