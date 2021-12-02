3b74c052464ac2fb350bc6e2ffe738dcfe08fad6
shivshankar31 <shivshankar31@yahoo.com>
2021-12-03 04:32:34 +0530

    124, 125, step 17, 18: add date through admin panel, config slug

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # step 16.1: register all three models in admin.py file to access the models on admin panel.
 # step 16.2: also add admin panel display class and add to register.
+# step 18.1: To create slug automaticly, add 'prepopulated_fields = {'slug': ('title',)}' field in admin.py 
 
 
 
@@ -9,7 +10,8 @@ from blog.models import Author, Post, Tag
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'excerpt', 'image_name', 'date', 'slug', 'content')
+    list_display = ('title', 'author','image_name', 'slug', 'date')
+    prepopulated_fields = {'slug': ('title',)} 
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
--- blog/models.py
+++ blog/models.py
@@ -15,11 +15,19 @@ from django.db.models.deletion import CASCADE, DO_NOTHING, SET_NULL
 class Tag(models.Model):
     caption = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f'{self.caption}'
 
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    def __str__(self):
+        return self.full_name()
 
 
 class Post(models.Model):
