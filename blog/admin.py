801b23f77e35f4e6613d4bcd179495fd7d3189aa
shivshankar31 <shivshankar31@yahoo.com>
2021-12-02 17:38:19 +0530

    120, 121, 122, 123: step 13, 14, 15, 16 add model, register and migrate

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+# step 16.1: register all three models in admin.py file to access the models on admin panel.
+# step 16.2: also add admin panel display class and add to register.
+
+
+
+from blog.models import Author, Post, Tag
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'excerpt', 'image_name', 'date', 'slug', 'content')
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('caption',)
+
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
+# step 13.1: add models Post in models.py file, with title, excerpt, image_name, date, slug, content.
+# Step 14.1: Add Author model in models.py with fields first_name, last_name, email
+# step 14.2: add author field in Post as models.foreignkey, it is used to mention (many to one relationship) field.
+# step 15.1: add Tag model in models.py with field caption.
+# step 15.2: add tags field to Post as models.manytomanyfield.
+
+
 from django.db import models
+from django.db.models.base import Model
+from django.core.validators import MinLengthValidator
+from django.db.models.deletion import CASCADE, DO_NOTHING, SET_NULL
 
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique = True)
+    content = models.TextField(validators= [MinLengthValidator(0)])
+    author = models.ForeignKey(Author, null= True, on_delete=models.SET_NULL, related_name='Post')
+    tags = models.ManyToManyField(Tag)
+
+
+
+
