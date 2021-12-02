daeae266d7c7fc255b7413eea6d18e6b0f9dba1c
shivshankar31 <shivshankar31@yahoo.com>
2021-12-03 05:17:05 +0530

    step 19: 126, 127: Fetch data from DB, display data

--- blog/views.py
+++ blog/views.py
@@ -4,94 +4,33 @@
 # step 10.1: add recent 3 post on the home page, from dummy data, also images are added
 # step 11.1: for all post page function is defind in views.py, to collect all post we use dict in render return  
 # step 11.3: In views.py, detail_post function use python next() function to return next item using for and if statements and return a key value
+# step 19.1: in view.py file - home page use objects.all and order by decending and slice to get new post
+# step 19.2: in view.py file - all post page, same as above use all() to display all post. 
+# step 19.3: in view.py file - detail post page, use get object 404 to display the single post.
+# step 19.5: remove all unused function and date from view.py file
 
-from datetime import date
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from blog.models import Post
 
 # Create your views here.
 
-
-allposts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "it.jpg",
-        "author": "Maximilia",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "it-2.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "it-3.jpeg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
-
-def get_date(post):
-    return post['date']
-
 def home(request):
-    shorted_post = sorted(allposts, key=get_date)
-    recent_post = shorted_post[-3:]
-    return render(request, 'blog/index.html', { 'post': recent_post })
+    latest_post = Post.objects.all().order_by("-date")[:3]
+    return render(request, 'blog/index.html', { 'post': latest_post })
 
 def post(request):
+    all_post = Post.objects.all().order_by('-date')
     return render(request, 'blog/all-posts.html', {
-        'allpost' : allposts
+        'allpost' : all_post
     })
 
 def detail_post(request, slug):
-    identified_post = next(post for post in allposts if post['slug'] == slug)
+    #method 1
+    # post_detail = Post.objects.get(slug = slug) 
+    # method 2 
+    post_detail = get_object_or_404(Post, slug = slug)
+
+    #identified_post = next(post for post in allposts if post['slug'] == slug)
     return render(request, 'blog/detail_post.html', {
-        'post_detail' : identified_post
-    })
+        'post_detail' : post_detail })
